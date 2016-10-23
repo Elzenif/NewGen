473937bbde36b8aa7535653cfc2325591a17a0ed
Elzenif <shigo.germain@gmail.com>
2016-10-23 14:50:27 +0200

    Fix fonts not loading and missing librairies in jar

--- generator/src/main/java/commons/Main.java
+++ generator/src/main/java/commons/Main.java
@@ -2,6 +2,8 @@ package commons;
 
 import commons.view.MainFrame;
 import commons.view.utils.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -12,6 +14,7 @@ import java.awt.FontFormatException;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.Enumeration;
 
 import static commons.view.utils.Constants.BENGUIAB_FONT;
@@ -25,29 +28,37 @@ import static commons.view.utils.Constants.LITHOGRL_FONT;
  */
 public class Main {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-      initFonts();
-      setUIFont(new FontUIResource(Constants.SOURCE_CODE_PRO, Font.PLAIN, Constants.FONT_SIZE_INT));
-    } catch (ClassNotFoundException | UnsupportedLookAndFeelException | InstantiationException | IllegalAccessException
-        | IOException | FontFormatException e) {
+    } catch (ClassNotFoundException | UnsupportedLookAndFeelException | InstantiationException | IllegalAccessException e) {
       e.printStackTrace();
     }
+    initFonts();
+    setUIFont(new FontUIResource(Constants.SOURCE_CODE_PRO, Font.PLAIN, Constants.FONT_SIZE_INT));
     SwingUtilities.invokeLater(MainFrame::new);
   }
 
   @SuppressWarnings({"SpellCheckingInspection", "HardCodedStringLiteral"})
-  private static void initFonts() throws IOException, FontFormatException {
-    DAUPHINN_FONT = initFont("DAUPHINN", 4f);
-    BENGUIAB_FONT = initFont("BENGUIAB", 0f);
-    LITHOGRL_FONT = initFont("LITHOGRL", 0f);
-    LITHOGRB_FONT = initFont("LITHOGRB", 0f);
+  private static void initFonts() {
+    try {
+      DAUPHINN_FONT = initFont("DAUPHINN", 4f);
+      BENGUIAB_FONT = initFont("BENGUIAB", 0f);
+      LITHOGRB_FONT = initFont("LITHOGRB", 0f);
+      LITHOGRL_FONT = initFont("LITHOGRL", 0f);
+    } catch (IOException | FontFormatException | URISyntaxException e) {
+      LOGGER.error("Cannot load fonts");
+      e.printStackTrace();
+    }
   }
 
-  private static Font initFont(String name, float sizeInc) throws IOException, FontFormatException {
-    InputStream is = new BufferedInputStream(Main.class.getClassLoader().getResourceAsStream("fonts/" + name + ".ttf"));
+  private static Font initFont(String name, float sizeInc) throws IOException, FontFormatException, URISyntaxException {
+    String pathname = "/fonts/" + name + ".TTF";
+    InputStream is = new BufferedInputStream(Thread.currentThread().getClass().getResourceAsStream(pathname));
     Font font = Font.createFont(Font.TRUETYPE_FONT, is);
+    is.close();
     return font.deriveFont(FONT_SIZE_FLOAT + sizeInc);
   }
 
@@ -61,5 +72,4 @@ public class Main {
       }
     }
   }
-
 }
